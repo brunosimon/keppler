5a9835dcbb91053ff7731a367d77290e133cc783
brunosimon <simon.bruno.77@gmail.com>
2016-06-08 01:45:54 +0200

    Start styling + Add syntaxic coloration

--- site/public/javascript/application/application.js
+++ site/public/javascript/application/application.js
@@ -1,3 +1,10 @@
-var application = angular.module( 'application', [] );
+var application = angular.module( 'application', [ 'hljs' ] );
 
 application.constant( 'config', config );
+application.config( function( hljsServiceProvider )
+{
+    hljsServiceProvider.setOptions({
+        // replace tab with 4 spaces
+        tabReplace: '    '
+    } );
+} );
