72a95b8904cf18fe4a74871b1e201d06e8ac5baa
brunosimon <simon.bruno.77@gmail.com>
2017-08-15 19:44:49 +0200

    :sparkles: Site > File tree > Add icons

--- site/src/components/files-tree-file/script.js
+++ site/src/components/files-tree-file/script.js
@@ -1,15 +1,18 @@
+import FileIcon from '@/components/file-icon/'
+
 export default
 {
     name: 'files-tree-file',
+
+    components:
+    {
+        FileIcon
+    },
 
     props:
     {
         depth: { type: Number, default: 0 },
         content: { type: Object }
-    },
-
-    created()
-    {
     },
 
     methods:
