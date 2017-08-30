9d450fc0a16d65246871274e35a16708b6b10a54
brunosimon <simon.bruno.77@gmail.com>
2017-08-30 22:48:24 +0200

    :sparkles: Site > File > Add download

--- site/src/components/file/script.js
+++ site/src/components/file/script.js
@@ -11,6 +11,14 @@ export default
         Viewer
     },
 
+    computed:
+    {
+        project()
+        {
+            return this.$store.state.projects.current
+        }
+    },
+
     data()
     {
         return {
