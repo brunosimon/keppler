84a4d37b44b88488ad6680fd6c9ebf684c18ca20
brunosimon <simon.bruno.77@gmail.com>
2017-12-27 15:42:31 +0100

    Updates

--- static/js/app.75f2cf29ecb1cf5b81cc.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([0],[function(o,e){console.log("Hello Keppler")}],[0]);
-//# sourceMappingURL=app.75f2cf29ecb1cf5b81cc.js.map
---
+++ static/js/app.d0806516803130b86448.js
@@ -0,0 +1,2 @@
+webpackJsonp([0],[function(n,e,t){"use strict";var o=t(3),c=t.n(o),s=function n(){c()(this,n),console.log("Hello Keppler")};e.a=s},function(n,e){},function(n,e,t){"use strict";Object.defineProperty(e,"__esModule",{value:!0});var o=t(1),c=(t.n(o),t(0));new c.a},function(n,e,t){"use strict";e.__esModule=!0,e.default=function(n,e){if(!(n instanceof e))throw new TypeError("Cannot call a class as a function")}}],[2]);
+//# sourceMappingURL=app.d0806516803130b86448.js.map
