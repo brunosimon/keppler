3e4dcd77b46f843eb07d6052267ce357b752a125
brunosimon <simon.bruno.77@gmail.com>
2017-12-27 22:23:54 +0100

    :construction: Site > Add header section

--- site/src/javascript/index.js
+++ site/src/javascript/index.js
@@ -1,7 +1,13 @@
+// import speaker from '../assets/videos/speaker.mp4'
+
 export default class Site
 {
     constructor()
     {
-        console.log('Hello Keppler')
+        // const $demo = document.querySelector('.js-demo')
+        // const $video = document.createElement('video')
+        // $video.src = speaker
+        // $video.controls = true
+        // $demo.appendChild($video)
     }
 }
