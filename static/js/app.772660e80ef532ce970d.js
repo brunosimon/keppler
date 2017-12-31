42fca03e640798ab77a00d946c2e59b1cd272ab9
brunosimon <simon.bruno.77@gmail.com>
2017-12-31 11:47:11 +0100

    Updates

--- static/js/app.772660e80ef532ce970d.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([0],[function(n,t,e){"use strict";var c=e(3),o=e.n(c),a=function n(){o()(this,n)};t.a=a},function(n,t){},function(n,t,e){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var c=e(1),o=(e.n(c),e(0));new o.a},function(n,t,e){"use strict";t.__esModule=!0,t.default=function(n,t){if(!(n instanceof t))throw new TypeError("Cannot call a class as a function")}}],[2]);
-//# sourceMappingURL=app.772660e80ef532ce970d.js.map
